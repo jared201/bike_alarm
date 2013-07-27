e3032697230035fbc6db39f181af4982cf8e82a2
Jared Odulio <jared.odulio@gmail.com>
2013-07-27 19:12:06 +0000

    added routing

--- bike_alarm.js
+++ bike_alarm.js
@@ -1,13 +1,33 @@
 if (Meteor.isClient) {
+
+  Meteor.Router.add({
+    '/': 'hello',
+
+    '/about': 'about',
+
+    '/contact': 'contact'
+    
+  });
+  Meteor.Router.to('hello');
+  Meteor.Router.page();
   Template.hello.greeting = function () {
     return "Welcome to bike_alarm.";
   };
 
   Template.hello.events({
-    'click input' : function () {
+    'click input.learn' : function () {
       // template data, if any, is available in 'this'
+    // $(".alert").alert('close')
+        console.log("You pressed the button");
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
+    },
+     'click button.learn' : function () {
+      // template data, if any, is available in 'this'
+    // $(".alert").alert('close')
+      Meteor.Router.to('about');
+      if (typeof console !== 'undefined')
+        console.log("You pressed the real  button");
     }
   });
 }
@@ -15,5 +35,6 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+   // Backbone.history.start({pushState: true});
   });
 }
