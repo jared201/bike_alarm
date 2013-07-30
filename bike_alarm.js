0ab5bf7886b9b5e48f937624fc5cba8db4c9ddc5
Jared Odulio <jared.odulio@gmail.com>
2013-07-30 05:05:13 +0000

    added new template

--- bike_alarm.js
+++ bike_alarm.js
@@ -1,18 +1,85 @@
 if (Meteor.isClient) {
 
+  Template.about.rendered = function () {
+     $("#aboutNav").addClass('active');
+   };
+
+  Template.about.destroyed = function () {
+     $("#aboutNav").removeClass('active');
+  };
+
+  Template.hello.rendered = function () {
+     $("#homeNav").addClass('active');
+   };
+
+  Template.hello.destroyed = function () {
+     $("#homeNav").removeClass('active');
+  };
+ 
+ 
+  Template.contact.destroyed = function () {
+     $("#contactNav").removeClass('active');
+  };
+
+  Template.locate.rendered = function () {
+     $("#locateNav").addClass('active');
+     var mapOptions = {
+        zoom: 16,
+       // center: new google.maps.LatLng(-34.397, 150.644),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+     
+  // Try HTML5 geolocation
+     if(navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+        var marker=new google.maps.Marker({
+           position:pos,
+           url: '/',
+           animation:google.maps.Animation.DROP
+        });
+        marker.setMap(map);
+        google.maps.event.addListener(marker, 'click', function() 
+          {window.location.href = marker.url;});
+   //     var infowindow = new google.maps.InfoWindow({
+   //      map: map,
+   //      position: pos,
+   //      content: 'Here we are!'
+   //    });
+
+       map.setCenter(pos);
+        }, function() {
+          handleNoGeolocation(true);
+       });
+      } else {
+      // Browser doesn't support Geolocation
+      handleNoGeolocation(false);
+      }
+  };
+
+  Template.locate.destroyed = function () {
+     $("#locateNav").removeClass('active');
+  };
   Meteor.Router.add({
+    
     '/': 'hello',
 
-    '/about': 'about',
-
-    '/contact': 'contact'
+    '/about':  'about',  
     
+    '/contact': 'contact',
+    
+    '/locate': 'locate' 
   });
-  //Meteor.Router.to('hello');
-  //Meteor.Router.page();
+
   Template.hello.greeting = function () {
     return "Welcome to bike_alarm.";
   };
+  
+
 
   Template.hello.events({
     'click input.learn' : function () {
@@ -32,7 +99,8 @@ if (Meteor.isClient) {
     }
   });
   Template.contact.rendered = function () {
-   
+     
+       $("#contactNav").addClass('active');
       var mapOptions = {
         zoom: 16,
        // center: new google.maps.LatLng(-34.397, 150.644),
@@ -70,6 +138,7 @@ if (Meteor.isClient) {
       handleNoGeolocation(false);
       }
   };
+
 }
 
 if (Meteor.isServer) {
